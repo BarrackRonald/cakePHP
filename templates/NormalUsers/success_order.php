80e44483d3b7580341386162ba280563a8a0f09d
Pham Van Hoan <phamhoan020501@gmail.com>
2021-09-29 17:35:25 +0700

    Chinh sưa lai gui mail, page hoan thanh va kiem tra trang thai login

---
+++ templates/NormalUsers/success_order.php
@@ -0,0 +1,24 @@
+
+<?php
+use Cake\Utility\Text;
+echo $this->element('NormalUsers/header');
+?>
+    <div class="product-big-title-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-bit-title text-center">
+                        <h2>Đặt hàng thành công</h2>
+                        <h4 style="color: #ccc;">Thông tin sản phẩm đã được gửi về mail của bạn!!!</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Page title area -->
+
+
+
+<?php
+echo $this->element('NormalUsers/footer');
+?>
+    
--- templates/element/NormalUsers/header.php
+++ templates/element/NormalUsers/header.php
@@ -1,3 +1,7 @@
+<?php
+$this->disableAutoLayout();
+
+?>
 <!DOCTYPE html>
 <!--
 	ustora by freshdesignweb.com
@@ -21,6 +25,7 @@
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="css/NormalUsers/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/NormalUsers/owl.carousel.css">
@@ -52,41 +57,25 @@
                             <li><a href="#"><i class="fa fa-heart"></i> Wishlist</a></li>
                             <li><a href="cart.html"><i class="fa fa-user"></i> My Cart</a></li>
                             <li><a href="checkout.html"><i class="fa fa-user"></i> Checkout</a></li>
-                            <li>
-                                <a href="/login">
-                                    <i class="fa fa-user"></i>
-                                   <?php  $session = $this->request->getSession();
-                                        if($session->check('username')){
-                                            echo $session->read('username');
-                                        }else{?>
-                                            login
-                                       <?php }
-                                   ?>
-                                </a>
-                            </li>
                         </ul>
                     </div>
                 </div>
-                
                 <div class="col-md-4">
-                    <div class="header-right">
-                        <ul class="list-unstyled list-inline">
-                            <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">currency :</span><span class="value">USD </span><b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">USD</a></li>
-                                    <li><a href="#">INR</a></li>
-                                    <li><a href="#">GBP</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">language :</span><span class="value">English </span><b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">English</a></li>
-                                    <li><a href="#">French</a></li>
-                                    <li><a href="#">German</a></li>
-                                </ul>
+                    <div class="user-menu">
+                        <ul>
+                            <li>
+                                <?php  $session = $this->request->getSession();
+                                    if($session->check('username')){?>
+                                        <a href="/logout">
+                                            <i class="fa fa-sign-out"></i>
+                                            Logout
+                                        </a>
+                                    <?php }else{?>
+                                        <a href="/login">
+                                            <i class="fa fa-sign-in"></i>
+                                            Login
+                                        </a>
+                                <?php } ?>
                             </li>
                         </ul>
                     </div>
@@ -94,7 +83,6 @@
             </div>
         </div>
     </div> <!-- End header area -->
-    
     <div class="site-branding-area">
         <div class="container">
             <div class="row">
@@ -153,7 +141,3 @@
         </div>
     </div> <!-- End mainmenu area -->
     
-<?php
-$this->disableAutoLayout();
-
-?>

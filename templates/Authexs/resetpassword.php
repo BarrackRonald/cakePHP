e5570fba8756d5d77cb951a70960c9ae8c958783
Pham Van Hoan <phamhoan020501@gmail.com>
2021-10-05 07:43:21 +0700

    Forgot password

--- templates/Authexs/resetpassword.php
+++ templates/Authexs/resetpassword.php
@@ -11,19 +11,22 @@
                         <div class="card-body">
                             <div class="text-center mb-5">
                                 <img src="../../img/Admin/favicon.svg" height="48" class='mb-4'>
-                                <h3>Forgot Password</h3>
+                                <h3>Reset Password</h3>
                                 <p>Please enter your email to receive password reset link.</p>
                             </div>
-                            <form action="index.html">
+                            <div class="row">
+                                <?= $this->Flash->render() ?>
+                            </div>
+                            <?= $this->Form->create() ?>
                                 <div class="form-group">
-                                    <label for="first-name-column">Email</label>
-                                    <input type="email" id="first-name-column" class="form-control" name="fname-column">
+                                    <label for="first-name-column">Password</label>
+                                    <input type="email" id="first-name-column" class="form-control" name="password">
                                 </div>
 
                                 <div class="clearfix">
                                     <button class="btn btn-primary float-end">Submit</button>
                                 </div>
-                            </form>
+                                <?= $this->Form->end() ?>
                         </div>
                     </div>
                 </div>
